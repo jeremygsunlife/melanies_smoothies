47567222e7dc6db567c1fdf9572d2f39e22f1a8e
jeremygsunlife <jeremy.goldsmith@sunlife.com>
2025-09-11 06:50:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,9 @@ name_on_order = st.text_input('Name on Smoothe:')
 st.write('The name on your Smoothie will be', name_on_order)
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name', col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 
 ingredients_list = st.multiselect(
